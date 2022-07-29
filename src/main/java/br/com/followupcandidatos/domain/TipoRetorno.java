fdb9ec538ff0d4911f12406b53a9e3a41cb67978
joao0506 <joaoholiveira1@hotmail.com>
2022-07-29 18:45:53 -0300

    Criando a tabela de Tipo de Retorno

---
+++ src/main/java/br/com/followupcandidatos/domain/TipoRetorno.java
@@ -0,0 +1,41 @@
+package br.com.followupcandidatos.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tipo_de_retorno")
+public class TipoRetorno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GenericGenerator(name = "INCREMENT", strategy = "INCREMENT")
+    @Column(columnDefinition = "SERIAL")
+    private Integer id;
+
+    @Column(length = 60)
+    private String descricao;
+
+    private Boolean isAtivo;
+
+    public TipoRetorno(Integer id, String descricao, Boolean isAtivo){
+        this.id = id;
+        this.descricao = descricao;
+        this.isAtivo = isAtivo;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"id\":" + id +
+                ",\"descricao\":\"" + descricao + "\"" +
+                ",\"isAtivo\":" + isAtivo +
+                '}';
+    }
+}
