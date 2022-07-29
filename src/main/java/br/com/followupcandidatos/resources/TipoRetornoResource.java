d70dc76be82a2f491a691ddeae4a07c95b335b0a
joao0506 <joaoholiveira1@hotmail.com>
2022-07-29 18:57:29 -0300

    Inserindo tipos de retorno

--- src/main/java/br/com/followupcandidatos/resources/TipoRetornoResource.java
+++ src/main/java/br/com/followupcandidatos/resources/TipoRetornoResource.java
@@ -1,9 +1,18 @@
 package br.com.followupcandidatos.resources;
 
+import br.com.followupcandidatos.domain.TipoRetorno;
+import br.com.followupcandidatos.domain.dtos.TipoRetornoDTO;
 import br.com.followupcandidatos.services.TipoRetornoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/tipo-retorno")
@@ -11,5 +20,13 @@ public class TipoRetornoResource {
 
     @Autowired
     private TipoRetornoService tipoRetornoService;
+
+    @PostMapping
+    public ResponseEntity<?> inserirTipoDeRetorno(@Valid @RequestBody TipoRetornoDTO tipoRetornoDTO){
+        TipoRetorno tipoRetorno = tipoRetornoService.salvarTipoDeRetorno(tipoRetornoService.fromDTO(tipoRetornoDTO));
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(tipoRetorno.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 
 }
--- src/main/java/br/com/followupcandidatos/services/TipoRetornoService.java
+++ src/main/java/br/com/followupcandidatos/services/TipoRetornoService.java
@@ -1,8 +1,13 @@
 package br.com.followupcandidatos.services;
 
+import br.com.followupcandidatos.domain.TipoRetorno;
+import br.com.followupcandidatos.domain.dtos.TipoRetornoDTO;
 import br.com.followupcandidatos.repositories.TipoRetornoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 public class TipoRetornoService {
@@ -10,4 +15,12 @@ public class TipoRetornoService {
     @Autowired
     private TipoRetornoRepository tipoRetornoRepository;
 
+    @Transactional
+    public TipoRetorno salvarTipoDeRetorno(TipoRetorno tipoRetorno) {
+        return tipoRetornoRepository.save(tipoRetorno);
+    }
+
+    public TipoRetorno fromDTO(TipoRetornoDTO tipoRetornoDTO) {
+        return new TipoRetorno(null, tipoRetornoDTO.getDescricao(), true);
+    }
 }
